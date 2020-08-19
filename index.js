9886a360e58d85bbd7747b88d60004660a1434e5
adityayadav800 <cooladityaamo@gmail.com>
2020-08-19 11:36:10 +0530

    First Commit :octocat:

---
+++ index.js
@@ -0,0 +1,37 @@
+    var y = Math.floor(Math.random() * 100 + 1); //Use of Math Object
+      
+    
+    var guess = 1; 
+    var guessHistory=[] //Use of Array Object
+    var date= new Date()//Use of Date Object
+    function GuessCheck(){ 
+      
+   // number guessed by user form Validation is done on this form object to check if it is a number.    
+   var x = document.getElementById("guessField").value; 
+  guessHistory.push(x);
+  if(guess< 10){
+   if(x == y) // Use of Boolean Objects
+   {     
+       alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+               + guess + " GUESS ON "+ date ); 
+   } 
+   else if(x > y) 
+   {     
+       guess++; 
+       alert("OOPS SORRY!! TRY A SMALLER NUMBER"); 
+   } 
+   else
+   { 
+       guess++; 
+       alert("OOPS SORRY!! TRY A GREATER NUMBER"); 
+   } 
+   updateGuessList();
+ }
+ else{
+    alert(" GUESSES ARE OVER AS OF"+ date+ "PLEASE REFRESH ");//Use of String Object
+  }
+   return false;
+} 
+function updateGuessList(){
+  document.getElementById("guessList").innerHTML=guessHistory.toString(); // Use of String Object.
+}
